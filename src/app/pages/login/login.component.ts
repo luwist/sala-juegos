24e10eb3c4af814ceb45ef2927f5734db3f34396
Javier Vilchez <vilchezjavierf@gmail.com>
2024-04-24 23:03:26 -0300

    create login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-login',
+    standalone: true,
+    imports: [
+        FormsModule,
+        InputTextModule,
+        PasswordModule,
+        ButtonModule
+    ],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css'
+})
+export class LoginComponent {
+    email: string = '';
+    password: string = '';
+
+    constructor(private router: Router) { }
+
+    login(): void {
+        console.log("AAAAAAA Login");
+
+        this.router.navigateByUrl('home');
+    }
+}
