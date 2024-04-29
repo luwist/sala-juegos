0e76cecccc3471383b2c859a1c5d9e192f5956ba
Javier Vilchez <vilchezjavierf@gmail.com>
2024-04-28 21:31:25 -0300

    add dropdown user

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,31 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login',
-    standalone: true,
-    imports: [
-        FormsModule,
-        InputTextModule,
-        PasswordModule,
-        ButtonModule
-    ],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss'
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    DropdownModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-    email: string = '';
-    password: string = '';
+export class LoginComponent implements OnInit {
+  users: any = [];
 
-    constructor(private router: Router) { }
+  selectedUser!: any;
 
-    login(): void {
-        console.log("AAAAAAA Login");
+  email: string = '';
+  password: string = '';
 
-        this.router.navigateByUrl('home');
-    }
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.users = [
+      {
+        avatar: 'assets/images/robot.png',
+        username: 'Luwist',
+        role: 'Administrador',
+        email: 'luwist@test.com',
+        password: '123456',
+      },
+      {
+        avatar: 'assets/images/robot.png',
+        username: 'StrogeBest',
+        role: 'Usuario',
+        email: 'strogebest@test.com',
+        password: '123456',
+      },
+      {
+        avatar: 'assets/images/robot.png',
+        username: 'Kwess',
+        role: 'Usuario',
+        email: 'strogebest@test.com',
+        password: '123456',
+      },
+      {
+        avatar: 'assets/images/robot.png',
+        username: 'ChesseTo',
+        role: 'Tester',
+        email: 'chesseto@test.com',
+        password: '123456',
+      },
+    ];
+  }
+
+  login(): void {
+    console.log('AAAAAAA Login');
+
+    this.router.navigateByUrl('home');
+  }
 }
