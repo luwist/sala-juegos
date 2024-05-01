8858cc5d916ba8fa71cbcdd95d54f5ad476f738d
Javier Vilchez <vilchezjavierf@gmail.com>
2024-05-01 18:38:12 -0300

    add box message

--- src/app/pages/landing/chat/chat.component.ts
+++ src/app/pages/landing/chat/chat.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
 export class ChatComponent {
+  message: string = '';
+  lastMessage!: string;
 
+  sendMessage(): void {
+    console.log('Enviar mensahe', this.message);
+  }
+
+  getKey(e: any): void {
+    if (e.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
 }
