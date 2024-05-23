3c09fb0e25808fa75af7f903f9b18bf402471fc2
Javier Vilchez <vilchezjavierf@gmail.com>
2024-05-23 18:21:42 -0300

    refactor code

--- src/app/pages/landing/games/higher-or-lower/higher-or-lower.component.ts
+++ src/app/pages/landing/games/higher-or-lower/higher-or-lower.component.ts
@@ -15,55 +15,88 @@ import {
   styleUrl: './higher-or-lower.component.scss',
 })
 export class HigherOrLowerComponent implements AfterViewInit {
-  @ViewChild('card') private _cardElement!: ElementRef;
-  @ViewChild('game') private _gameElement!: ElementRef;
+  // @ViewChild('card') private _cardElement!: ElementRef;
+  // @ViewChild('game') private _gameElement!: ElementRef;
 
-  @ViewChild('gameplay') private _gameplay!: ElementRef;
-  @ViewChild('endgame') private _endgame!: ElementRef;
+  // @ViewChild('gameplay') private _gameplay!: ElementRef;
+  // @ViewChild('endgame') private _endgame!: ElementRef;
+  // @ViewChild('containerButtons') private _containerButtons!: ElementRef;
 
-  private _currentCard!: number;
+  // private _currentCard!: number;
+  // totalCard = 48;
 
   ngAfterViewInit(): void {
-    this._currentCard = this.randomCard();
+    // this._currentCard = this.randomCard();
   }
 
-  higherSelected(): void {
-    const card = this.randomCard();
+  // higherSelected(e: any): void {
+  //   const buttonElement = e.target as HTMLButtonElement;
+  //   console.log(e);
+  //   console.log(e.target);
+  //   console.log(buttonElement);
 
-    if (this._currentCard < card) {
-      this.showEnd('welldone', '¡Ganaste!');
-    } else {
-      this.showEnd('gameover', 'Perdiste');
-    }
-  }
+  //   const card = this.randomCard();
 
-  lowerSelected(): void {
-    const card = this.randomCard();
+  //   if (this._currentCard < card) {
+  //     buttonElement.classList.add('right');
 
-    if (this._currentCard > card) {
-      this.showEnd('welldone', '¡Ganaste!');
-    } else {
-      this.showEnd('gameover', 'Perdiste');
-    }
-  }
+  //     // this.showEnd('welldone', '¡Ganaste!');
+  //   } else {
+  //     buttonElement.classList.add('wrong');
 
-  private showEnd(className: string, message: string): void {
-    const element = this._gameElement.nativeElement;
+  //     // this.showEnd('gameover', 'Perdiste');
+  //   }
 
-    element.classList.add('show');
-    element.childNodes[0].classList.add(className);
-    element.childNodes[0].childNodes[0].innerText = message;
-  }
+  //   setTimeout(() => {
+  //     this.resetButtons();
+  //   }, 500);
+  // }
 
-  private randomCard(): number {
-    const element = this._cardElement.nativeElement;
+  // lowerSelected(e: any): void {
+  //   const buttonElement = e.target as HTMLButtonElement;
 
-    let randomCard = Math.floor(Math.random() * 13 - 0 + 0);
-    let x = randomCard * 92;
-    let y = randomCard * 128;
+  //   const card = this.randomCard();
 
-    element.style.backgroundPosition = `-${x}px ${y}px`;
+  //   if (this._currentCard > card) {
+  //     buttonElement.classList.add('right');
+  //   } else {
+  //     buttonElement.classList.add('wrong');
+  //   }
 
-    return randomCard;
-  }
+  //   setTimeout(() => {
+  //     this.resetButtons();
+  //   }, 500);
+  // }
+
+  // resetButtons(): void {
+  //   const containerButtons = this._containerButtons
+  //     .nativeElement as HTMLDivElement;
+
+  //   for (let i = 0; i < containerButtons.children.length; i++) {
+  //     const buttonElement = containerButtons.children[i] as HTMLButtonElement;
+
+  //     buttonElement.classList.remove('right');
+  //     buttonElement.classList.remove('wrong');
+  //   }
+  // }
+
+  // private showEnd(className: string, message: string): void {
+  //   const element = this._gameElement.nativeElement;
+
+  //   element.classList.add('show');
+  //   element.childNodes[0].classList.add(className);
+  //   element.childNodes[0].childNodes[0].innerText = message;
+  // }
+
+  // private randomCard(): number {
+  //   const element = this._cardElement.nativeElement;
+
+  //   let randomCard = Math.floor(Math.random() * 13 - 0 + 0);
+  //   let x = randomCard * 92;
+  //   let y = randomCard * 128;
+
+  //   element.style.backgroundPosition = `-${x}px ${y}px`;
+
+  //   return randomCard;
+  // }
 }
